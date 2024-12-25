3d217f4c763d9fac71e498a713eb9f471a9a283f
sayed udoy <umudoy364@gmail.com>
2024-12-25 15:45:52 +0500

    location section heading done

--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import ContentHeading from '../Global/ContentHeading'
+import Button from '../Global/Button'
+import { FaPlus } from 'react-icons/fa'
 
 function Locations() {
   return (
-    <div>Locations</div>
+    <section>
+      <ContentHeading headingTitle="DR-4699 March 2023 Severe Storms" isSearch={true} isSort={true}>
+        <Button classNm="flex items-center justify-center bg-primaryColor text-white gap-2">
+          <FaPlus />
+          New Location
+        </Button>
+      </ContentHeading>
+    </section>
   )
 }
 
