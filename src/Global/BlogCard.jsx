b2c2be3634efd64f8326c03a3038f3157337cfab
sayed udoy <umudoy364@gmail.com>
2024-12-25 21:51:16 +0500

    i have changed some style

--- src/Global/BlogCard.jsx
+++ src/Global/BlogCard.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function BlogCard({data,flexRow}) {
   return (
-    <div className={`${flexRow && "flex gap-3 items-center bg-[#F4F4F5] p-2 rounded-md"}`}>
-      <div className={`${flexRow && "w-[5rem] h-[5rem]"} `}>
-        <img src={data.image} alt="" />
+    <div className={`${flexRow && "flex gap-3 items-center bg-[#F4F4F5] p-2 rounded-md"} cursor-pointer group `}>
+      <div className={`${flexRow && "w-[5rem] h-[5rem]"} overflow-hidden`}>
+        <img className="group-hover:scale-105 transition duration-500" src={data.image} alt="" />
       </div>
       <div className="pb-2">
         <h2 className="text-[16px] font-bold">{data.title}</h2>
